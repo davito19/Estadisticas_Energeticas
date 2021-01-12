6ac5492e934bfe768def2c2d154524eadd57a8f8
Davito19 <omardbonett@gmail.com>
2021-01-12 00:37:35 -0500

    version final main

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+import ventana as ven
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main():
+    mi_app = ven.Aplicacion()
+    return 0
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+main()
